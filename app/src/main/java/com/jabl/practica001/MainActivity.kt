3c2effd1ea7361cfa5204b1248d74393a9a91846
Sergio García <sergio.garcia@triplei.mx>
2020-05-07 20:01:00 -0500

    prueba

--- app/src/main/java/com/jabl/practica001/MainActivity.kt
+++ app/src/main/java/com/jabl/practica001/MainActivity.kt
@@ -2,11 +2,14 @@ package com.jabl.practica001
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"mensaje",Toast.LENGTH_LONG).show()
+
     }
 }
